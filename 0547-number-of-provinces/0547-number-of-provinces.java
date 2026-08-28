31230201b9b41ba969ba90104ecb9c24949769e9
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-08-29 01:24:53 +0530

    Time: 11 ms (5.23%), Space: 49.2 MB (17.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,18 @@
 class Solution {
+    private void bfs(List<Integer>[] graph, int idx, boolean[] vis){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(idx);
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            vis[curr]=true;
+            for(int i=0;i<graph[curr].size();i++){
+                int next=graph[curr].get(i);
+                if(!vis[next]){
+                    q.offer(next);
+                }
+            }
+        }
+    }
     private void dfs(List<Integer>[] graph, int idx, boolean[] vis){
         vis[idx]=true;
 
@@ -27,7 +41,8 @@ class Solution {
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 ans++;
-                dfs(graph,i,vis);
+                // dfs(graph,i,vis);
+                bfs(graph,i,vis);
             }
         }
 
