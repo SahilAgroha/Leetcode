a60c131507f2115c2fc71d55bc4453b583ff2e4d
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-09-15 22:09:26 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -11,9 +11,23 @@ class Solution {
         }
         return way[n]=way[n-1]+way[n-2];
     }
+    private int helper2(int n,int[] way){
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        way[0]=way[1]=1;
+
+        for(int i=2;i<=n;i++){
+            way[i]=way[i-1]+way[i-2];
+        }
+
+        return way[n];
+    }
     public int climbStairs(int n) {
         int way[]=new int[n+1];
-        return helper(n,way);
+        // return helper(n,way);
+        return helper2(n,way);
     }
 
 }
