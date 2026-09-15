32fd79848362d556fc628f5f82fe8d5365029561
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-09-15 23:03:31 +0530

    Time: 1 ms (38.56%), Space: 46.9 MB (42.79%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        helper(image,sr,sc,image[sr][sc],color);
-        return image;
+    private void helper(int[][] image, int sr, int sc, int color, int startColor){
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc]!=startColor){
+            return ;
+        }
+
+        image[sr][sc]=color;
+        int dx[]={1,0,-1,0};
+        int dy[]={0,1,0,-1};
+
+        for(int i=0;i<4;i++){
+            int nr=sr+dx[i];
+            int nc=sc+dy[i];
+            helper(image,nr,nc,color,startColor);
+        }
+
     }
-    private void helper(int[][] image, int sr, int sc,int orgCol, int color){
-        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || orgCol==color || image[sr][sc]!=orgCol){
-            return;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int startColor = image[sr][sc];
+
+        if (startColor == color) {
+            return image;
         }
-        if(image[sr][sc]==orgCol){
-            image[sr][sc]=color;
-        }
-        helper(image,sr,sc-1,orgCol,color);
-        helper(image,sr,sc+1,orgCol,color);
-        helper(image,sr-1,sc,orgCol,color);
-        helper(image,sr+1,sc,orgCol,color);
+
+        helper(image,sr,sc,color,startColor);
+        return image;
     }
 }
