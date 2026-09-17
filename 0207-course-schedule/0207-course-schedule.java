6a5fa54f0294a4f9e4c16fc756b7aa348f9e290e
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-09-17 23:33:51 +0530

    Time: 6 ms (72.44%), Space: 47.1 MB (64.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,35 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] graph=new ArrayList[n];
-
+        int inDeg[]=new int[n];
+        int count=0;
+        ArrayList<Integer>[] graph=new ArrayList[n];
         for(int i=0;i<n;i++){
             graph[i]=new ArrayList<>();
         }
-
-        int[] indeg=new int[n];
-
-        for(int[] pre:prerequisites){
-            indeg[pre[0]]++;
+        for(int[] pre: prerequisites){
+            inDeg[pre[0]]++;
             graph[pre[1]].add(pre[0]);
         }
 
         Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(indeg[i]==0){
+            if(inDeg[i]==0){
                 q.offer(i);
             }
         }
 
-        int comp=0;
         while(!q.isEmpty()){
-            comp++;
             int curr=q.poll();
-
-            for(int next:graph[curr]){
-                indeg[next]--;
-                if(indeg[next]==0){
+            count++;
+            for(int next : graph[curr]){
+                inDeg[next]--;
+                if(inDeg[next]==0){
                     q.offer(next);
                 }
             }
         }
 
-        return comp==n;
+        return count==n;
+
     }
 }
