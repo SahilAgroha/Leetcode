3379f56444e89b228b824d788bcc29838f36a7c9
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-09-20 00:00:01 +0530

    Time: 0 ms (100%), Space: 44.9 MB (81.96%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,26 +1,27 @@
 class Solution {
-    private int findPar(int x,int[] par){
-        if(par[x]==x){
-            return x;
+    private int findPar(int[] par, int idx){
+        if(par[idx]==idx){
+            return par[idx];
         }
-        return findPar(par[x],par);
+        return par[idx]=findPar(par, par[idx]);
     }
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length;
-        int[] par=new int[n+1];
-        for(int i=0;i<=n;i++){
+        int[] par=new int[n];
+        for(int i=0;i<n;i++){
             par[i]=i;
         }
 
-        for(int[] edge : edges){
-            int par1=findPar(edge[0],par);
-            int par2=findPar(edge[1],par);
-            if(par1==par2){
-                return edge;
+        for(int[] e: edges){
+            int parA=findPar(par,e[0]-1);
+            int parB=findPar(par,e[1]-1);
+            if(parA==parB){
+                return e;
             }
-            par[par1]=par2;
+
+            par[parB]=parA;
         }
 
-        return new int[2];
+        return new int[]{-1,-1};
     }
 }
