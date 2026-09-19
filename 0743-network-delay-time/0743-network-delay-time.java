8bb18bd2cb6612f9728edcbefdb3372b2ec6f43e
Sahil <152394116+SahilAgroha@users.noreply.github.com>
2026-09-19 23:10:55 +0530

    Time: 10 ms (81.57%), Space: 48.7 MB (76.53%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,49 +1,48 @@
 class Solution {
     class Edge{
-        int src;
-        int des;
-        int time;
-        Edge(int s,int d, int t){
-            this.src=s;
-            this.des=d;
-            this.time=t;
+        int u;
+        int v;
+        int w;
+        Edge(int u, int v, int w){
+            this.u=u; 
+            this.v=v;
+            this.w=w;
         }
     }
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<Edge>[] graph=new ArrayList[n];
+        ArrayList<Edge>[] graph=new ArrayList[n];
         for(int i=0;i<n;i++){
             graph[i]=new ArrayList<>();
         }
+
         for(int[] t:times){
-            int u=t[0]-1;
-            int v=t[1]-1;
-            int w=t[2];
-            graph[u].add(new Edge(u,v,w));
+            graph[t[0]-1].add(new Edge(t[0]-1,t[1]-1, t[2] ));
         }
         k--;
-        int[] time=new int[n];
+        int dist[]=new int[n];
         for(int i=0;i<n;i++){
             if(i!=k){
-                time[i]=Integer.MAX_VALUE;
+                dist[i]=Integer.MAX_VALUE;
             }
         }
         Queue<Integer> q=new LinkedList<>();
         q.offer(k);
         while(!q.isEmpty()){
             int curr=q.poll();
-            for(int i=0;i<graph[curr].size();i++){
-                Edge next=graph[curr].get(i);
-                if(time[curr]+next.time<time[next.des]){
-                    time[next.des]=time[curr]+next.time;
-                    q.offer(next.des);
+            for(Edge e:graph[curr]){
+                if(dist[curr]+e.w<dist[e.v]){
+                    dist[e.v]=dist[curr]+e.w;
+                    q.offer(e.v);
                 }
             }
         }
-        int max=0;
-        for(int i=0;i<n;i++){
-            max=Math.max(max,time[i]);
+
+        int max=dist[0];
+        for(int i=1;i<n;i++){
+            max=Math.max(max,dist[i]);
         }
 
         return max==Integer.MAX_VALUE?-1:max;
+
     }
 }
